772a1da35fa94d603621762aebda8ba32c73bed0
Nicola Bonelli <nicola.bonelli@gmail.com>
2011-11-10 13:12:44 +0100

    runme shell script updated.

--- driver/ixgbe-3.3.9/src/runme.sh
+++ driver/ixgbe-3.3.9/src/runme.sh
@@ -1,5 +1,27 @@
-#!/bin/sh
-set -e
-cp ../../../kernel/Module.symvers .
-make -j12
+#!/bin/bash
 
+set -e
+
+PROGNAME=$(basename $0)
+
+MODSYM=../../../kernel/Module.symvers
+
+function error_exit
+{
+    echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+    exit 1
+}
+
+if [ ! -r ${MODSYM} ]; then
+    error_exit "Compile PFQ kernel module first!"    
+fi 
+
+/bin/cp ${MODSYM} .
+
+echo "[*] Compiling the PFQ aware driver..."
+
+if [ -x /usr/bin/colormake ]; then 
+    /usr/bin/colormake V=1 -j12
+else   
+    /usr/bin/make V=1 -j12
+fi
