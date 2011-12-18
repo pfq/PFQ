0cfe80b50a2c19f30c96e692ed84cf5015afeea9
Nicola Bonelli <nicola.bonelli@gmail.com>
2011-12-18 14:17:28 +0100

    Minor changes.

--- user/test/test-tstamp.cpp
+++ user/test/test-tstamp.cpp
@@ -8,12 +8,7 @@ using namespace net;
 int
 main(int argc, char *argv[])
 {
-    if (argc < 2)
-        throw std::runtime_error(std::string("usage: ").append(argv[0]).append(" dev"));
-    
     pfq q(pfq_open);
-
-    q.add_device(argv[1], pfq::any_queue);
 
     q.tstamp(true);
     
