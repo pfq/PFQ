fee250590085ff55a715cb8eb8a79bd358fba791
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-01-04 13:15:49 +0100

    Fixed licenses: GPL for the Linux kernel module, BSD for the user-space library.

--- kernel/pf_q-global.h
+++ kernel/pf_q-global.h
@@ -1,3 +1,27 @@
+/***************************************************************
+ *                                                
+ * (C) 2011-12 Nicola Bonelli <nicola.bonelli@cnit.it>   
+ *             Andrea Di Pietro <andrea.dipietro@for.unipi.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ ****************************************************************/
+
 #ifndef _PF_Q_GLOBAL_H_
 #define _PF_Q_GLOBAL_H_ 
 
