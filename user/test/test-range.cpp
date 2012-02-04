dae61740867c30e4fb433df2202b8368cc3d6276
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-02-04 21:11:22 +0100

    Added C++11 for-range loop test.

---
+++ user/test/test-range.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <string>
+#include <stdexcept>
+
+#include <pfq.hpp>
+using namespace net;
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 2)
+        throw std::runtime_error(std::string("usage: ").append(argv[0]).append(" dev"));
+    
+    pfq r(1514);
+
+    r.add_device(argv[1], pfq::any_queue);
+
+    r.tstamp(true);
+    
+    r.enable();
+    
+    for(;;)
+    {
+            auto many = r.read( 1000000 /* timeout: micro */);
+            
+            std::cout << "batch size: " << many.size() << " ===>" << std::endl;
+
+            for(auto & packet : many)
+            {
+                while(!packet.commit);
+
+                // printf("caplen:%d len:%d ifindex:%d hw_queue:%d tstamp: %u:%u -> ", it->caplen, it->len, it->if_index, it->hw_queue,
+                //                                                                    it->tstamp.tv.sec, it->tstamp.tv.nsec);
+                char *buff = static_cast<char *>(data(packet));
+
+                for(int x=0; x < std::min<int>(packet.caplen, 34); x++)
+                {
+                    printf("%2x ", (unsigned char)buff[x]);
+                }
+                printf("\n");
+            }
+    }
+
+    return 0;
+}
+ 
