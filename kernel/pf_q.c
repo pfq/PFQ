58c88bf1c93c9f2e5780b81b2255c16a1c4a1f5d
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-04-07 11:51:04 +0200

    Removed unused skb_frag functions

--- kernel/pf_q.c
+++ kernel/pf_q.c
@@ -95,25 +95,6 @@ pfq_sk(struct sock *sk)
 }
 
 
-inline void *kmap_skb_frag(const skb_frag_t *frag)
-{
-#ifdef CONFIG_HIGHMEM
-        BUG_ON(in_irq());
-
-        local_bh_disable();
-#endif
-        return kmap_atomic(frag->page, KM_SKB_DATA_SOFTIRQ);
-}
-
-inline void kunmap_skb_frag(void *vaddr)
-{
-        kunmap_atomic(vaddr, KM_SKB_DATA_SOFTIRQ);
-#ifdef CONFIG_HIGHMEM
-        local_bh_enable();
-#endif
-}
-
-
 inline 
 int pfq_get_free_id(struct pfq_opt * pq)
 {
