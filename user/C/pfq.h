f022db16921be47d6b0e648a2f1b477e4f9b58e7
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-04-09 13:36:47 +0200

    Added experimental support of PFQ C library

---
+++ user/C/pfq.h
@@ -0,0 +1,74 @@
+/***************************************************************
+   
+   Copyright (c) 2012, Nicola Bonelli 
+   All rights reserved. 
+
+   Redistribution and use in source and binary forms, with or without 
+   modification, are permitted provided that the following conditions are met: 
+
+   * Redistributions of source code must retain the above copyright notice, 
+     this list of conditions and the following disclaimer. 
+   * Redistributions in binary form must reproduce the above copyright 
+     notice, this list of conditions and the following disclaimer in the 
+     documentation and/or other materials provided with the distribution. 
+   * Neither the name of University of Pisa nor the names of its contributors 
+     may be used to endorse or promote products derived from this software 
+     without specific prior written permission. 
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+   POSSIBILITY OF SUCH DAMAGE.
+ 
+ ***************************************************************/
+
+#ifndef _PFQ_H_
+#define _PFQ_H_ 
+
+#include <stddef.h>
+#include <linux/pf_q.h>
+
+/* placeholder type for pfq descriptor */
+
+typedef void * pfq_t;
+
+typedef void (*pfq_handler)(char *user, const struct pfq_hdr *h, const char *data); 
+
+extern pfq_t pfq_default();
+extern pfq_t pfq_open(size_t calpen, size_t offset, size_t slots);
+extern void  pfq_close(pfq_t *);
+extern const char *pfq_error(pfq_t *);
+
+extern void pfq_enable(pfq_t *q, int *ok);
+extern int  pfq_disable(pfq_t *q, int *ok);
+extern int  pfq_is_enabled(pfq_t const *q, int *ok);
+
+extern void pfq_load_balance(pfq_t *q, int value, int *ok);
+extern int pfq_ifindex(pfq_t const *q, const char *dev, int *ok);
+extern void pfq_set_tstamp(pfq_t *q, int value, int *ok);
+extern int pfq_get_tstamp(pfq_t const *q, int *ok);
+extern void pfq_set_caplen(pfq_t *q, size_t value, int *ok);
+extern size_t pfq_get_caplen(pfq_t const *q, int *ok);
+extern void pfq_set_offset(pfq_t *q, size_t value, int *ok);
+extern size_t pfq_get_offset(pfq_t const *q, int *ok);
+extern void pfq_set_slots(pfq_t *q, size_t value, int *ok);
+extern size_t pfq_get_slots(pfq_t const *q, int *ok);
+extern size_t pfq_get_slot_size(pfq_t const *q, int *ok);
+extern void pfq_add_device_by_index(pfq_t *q, int index, int queue, int *ok);
+extern void pfq_add_device_by_name(pfq_t *q, const char *dev, int queue,int *ok);
+extern void pfq_remove_device_by_index(pfq_t *q, int index, int queue, int *ok);
+extern void pfq_remove_device_by_name(pfq_t *q, const char *dev, int queue, int *ok);
+extern int pfq_poll(pfq_t *q, long int usec, int *ok);
+extern int pfq_id(pfq_t const *q, int *ok);
+extern struct pfq_stats pfq_get_stats(pfq_t const *q, int *ok);
+
+extern int pfq_dispatch(pfq_t *q, pfq_handler callback, char *user, int *ok);
+
+#endif /* _PFQ_H_ */
---
+++ user/C/test-dispatch.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pfq.h>
+
+#define MIN(a,b) (a < b ? a : b)
+
+void dispatch(char *user, const struct pfq_hdr *h, const char *data)
+{
+        int x;
+        for(x = 0; x < MIN(h->caplen,34); x++)
+        {
+                printf("%2x ", (unsigned char)data[x]);
+        }
+        printf("\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+        int ok = 0;
+
+        if (argc < 2) {
+                fprintf(stderr, "usage: %s dev\n", argv[0]);
+                return 0;
+        }
+
+        pfq_t *p = pfq_default();
+        if (p == NULL) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        pfq_enable(p, &ok);
+        if (!ok) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        int enabled = pfq_is_enabled(p, &ok);
+        if (!ok) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        int caplen = pfq_get_caplen(p, &ok);
+        if (!ok) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        printf("caplen: %d\n", caplen);
+
+        int offset = pfq_get_offset(p, &ok);
+        if (!ok) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        printf("offset: %d\n", offset);
+
+        int id = pfq_id(p, &ok);
+        if (!ok) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        printf("id: %d\n", id);
+
+        pfq_add_device_by_name(p, "eth0", -1, &ok);
+
+        int n = 0;
+        for(;n < 100; n++) {
+                int n = pfq_dispatch(p, dispatch, NULL, &ok);
+                if (!ok) {
+                        printf("error: %s\n", pfq_error(p));
+                }
+        }
+
+        struct pfq_stats s = pfq_get_stats(p, &ok);
+        if (!ok) {
+                printf("error: %s\n", pfq_error(p));
+                return -1;
+        }
+
+        printf("stats:: recv=%d lost=%d drop=%d\n", s.recv, s.lost, s.drop);
+
+        pfq_close(p);
+        return 0;
+}
+
