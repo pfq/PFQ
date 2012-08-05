c2fa597061693f8d50bece069834ad289f118c75
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-08-05 14:41:05 +0200

    Added steering-dummy test

---
+++ user/test/test-steering-dummy.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <string>
+#include <stdexcept>
+
+#include <pfq.hpp>
+using namespace net;
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 2)
+        throw std::runtime_error(std::string("usage: ").append(argv[0]).append(" dev"));
+    
+    pfq r(128);
+
+    r.bind(argv[1], pfq::any_queue);
+
+    r.toggle_time_stamp(true);
+    
+    r.set_steering_function(r.group_id(), "steer-dummy");
+
+    r.enable();
+    
+    for(int i = 0; i < 5;++i)
+    {
+            r.read( 1000000 /* timeout: micro */);
+    }
+    
+    int state = 42;
+    r.set_group_state(r.group_id(), state); 
+
+    for(int i = 0; i < 5;++i)
+    {
+            r.read( 1000000 /* timeout: micro */);
+    }
+
+    r.set_group_state(r.group_id()); 
+    
+    for(int i = 0; i < 5;++i)
+    {
+            r.read( 1000000 /* timeout: micro */);
+    }
+
+    return 0;
+}
+ 
