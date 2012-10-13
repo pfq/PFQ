930fbeb1bbf912e93fb878faef9c116f56115169
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-10-13 12:47:50 +0200

    Simple readQ test updated

--- test-dump.hs
+++ test-dump.hs
@@ -1,9 +1,20 @@
 import PFq 
 
+import Foreign
+
+recv :: Ptr PFqTag -> IO ()
+recv q = do 
+    queue <- PFq.readQ q 10000
+    if ( PFq.queueLen(queue) == 0 ) then recv q else (print queue >> recv q)
+
 xxx :: IO ()
 xxx = do
-        q <- PFq.open 100 100 100
-        return ()
+    fp <- PFq.open 64 0 4096
+    withForeignPtr fp  $ \q -> do
+        PFq.enable q 
+        PFq.bind q "eth0" (-1)
+        recv q
+    return ()
 
 main = do
     xxx >> (print "done")
