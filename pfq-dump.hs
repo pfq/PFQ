39d78762326b90c5f04fb615efd3943e6c124d4f
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-10-15 10:59:26 +0200

    PFq imported qualified as Q

--- pfq-dump.hs
+++ pfq-dump.hs
@@ -1,42 +1,42 @@
-import PFq 
+import PFq as Q
 import Foreign
 import System.Environment
 
 import Control.Monad
 
-handler :: PFq.Callback
+handler :: Q.Callback
 handler h _ = print h
 
 recvDispatch :: Ptr PFqTag -> IO()
 recvDispatch q = do 
-        PFq.dispatch q handler 1000 
+        Q.dispatch q handler 1000 
         recvDispatch q
 
 recvLoop :: Ptr PFqTag -> IO ()
 recvLoop q = do 
-    queue <- PFq.readQ q 10000
-    if ( PFq.queueLen(queue) == 0 ) 
+    queue <- Q.read q 10000
+    if ( Q.queueLen(queue) == 0 ) 
        then recvLoop q 
        else do
-            hs <- PFq.getHeaders queue
+            hs <- Q.getHeaders queue
             mapM_ print hs
-            gid <- PFq.getGroupId q
-            PFq.getStats q >>= print
-            -- putStrLn $ "qlen: " ++ show(PFq.queueLen(queue)) ++ " hlen: " ++ show(length hs)
+            gid <- Q.getGroupId q
+            Q.getStats q >>= print
+            -- putStrLn $ "qlen: " ++ show(Q.queueLen(queue)) ++ " hlen: " ++ show(length hs)
             recvLoop q
 
 dumper :: String -> IO ()
 dumper dev = do
     putStrLn  $ "dumping " ++ dev  ++ "..."
-    fp <- PFq.open 64 14 4096
+    fp <- Q.open 64 14 4096
     withForeignPtr fp  $ \q -> do
-        PFq.setTimestamp q True
-        --PFq.bind q dev (-1)
-        gid <- PFq.getGroupId q
-        PFq.bindGroup q gid dev (-1)
-        PFq.enable q 
-        PFq.steeringFunction q gid "steer-ipv4-addr"
-        PFq.getSlotSize q >>= \o -> putStrLn $ "slot_size: " ++ show(o)
+        Q.setTimestamp q True
+        --Q.bind q dev (-1)
+        gid <- Q.getGroupId q
+        Q.bindGroup q gid dev (-1)
+        Q.enable q 
+        Q.steeringFunction q gid "steer-ipv4-addr"
+        Q.getSlotSize q >>= \o -> putStrLn $ "slot_size: " ++ show(o)
         -- recvLoop q
         recvDispatch q
 
