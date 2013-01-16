59a9285919a1484fcfc4c3da88a631622dc753e4
Nicola Bonelli <nicola.bonelli@cnit.it>
2013-01-16 18:13:04 +0100

    Fix: bug when if_index exceed the Q_MAX_DEVICE size

--- kernel/pf_q-devmap.c
+++ kernel/pf_q-devmap.c
@@ -73,7 +73,7 @@ int pfq_devmap_update(int action, int index, int queue, int gid)
         {
             unsigned long tmp;
 
-            if ( !__pfq_devmap_equal(i,q,index,queue) )
+            if ( !__pfq_devmap_equal(i,q, index & Q_MAX_DEVICE_MASK, queue) )
                 continue;
 
             /* map_set... */
