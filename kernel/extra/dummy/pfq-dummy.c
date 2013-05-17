2d5a812c5289e95c8aa8b57a2640f1b79e1d9f68
Nicola Bonelli <nicola.bonelli@cnit.it>
2013-05-17 14:03:19 +0200

    Extra: dummy package updated.

---
+++ kernel/extra/dummy/pfq-dummy.c
@@ -0,0 +1,45 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include <linux/pf_q.h>
+#include <linux/pf_q-fun.h>
+ 
+MODULE_LICENSE("GPL");
+
+ret_t
+steering_dummy(struct sk_buff *skb, ret_t ret)
+{
+	sk_function_t fun = get_next_function(skb);
+
+        if (is_skip(ret) || is_drop(ret))
+                return pfq_call(fun, skb, ret);
+
+	/* perform action here */
+
+        return pfq_call(fun, skb, ret);
+}
+
+
+struct sk_function_descr hooks[] = {
+	{ "steer-dummy", steering_dummy },
+	{ NULL, NULL}};
+
+
+
+static int __init usr_init_module(void)
+{
+	return pfq_register_functions("[dummy]", hooks);
+}
+
+
+static void __exit usr_exit_module(void)
+{                 
+	pfq_unregister_functions("[dummy]", hooks);
+}
+	
+
+module_init(usr_init_module);
+module_exit(usr_exit_module);
+
+
