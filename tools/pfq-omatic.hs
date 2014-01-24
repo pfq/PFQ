81b53d012e3f6c99bd23042658ebc589870ffe87
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-01-24 11:32:17 +0100

    Minor changes in pfq-omatic.

--- tools/pfq-omatic.hs
+++ tools/pfq-omatic.hs
@@ -1,35 +1,36 @@
 --
 --
---  (C) 2011-13 Nicola Bonelli <nicola.bonelli@cnit.it>   
--- 
+--  (C) 2011-13 Nicola Bonelli <nicola.bonelli@cnit.it>
+--
 --  This program is free software; you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
 --  the Free Software Foundation; either version 2 of the License, or
 --  (at your option) any later version.
--- 
+--
 --  This program is distributed in the hope that it will be useful,
 --  but WITHOUT ANY WARRANTY; without even the implied warranty of
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU General Public License for more details.
--- 
+--
 --  You should have received a copy of the GNU General Public License
 --  along with this program; if not, write to the Free Software Foundation,
 --  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
--- 
+--
 --  The full GNU General Public License is included in this distribution in
 --  the file called "COPYING".
- 
+
 
 import System.IO.Unsafe
 import System.Process
 import System.Directory
 import System.FilePath
 import Control.Monad(when,unless,forM)
+import Control.Applicative
 import Text.Regex.Posix
 
 import Data.List
 
-pfq_omatic_ver,pfq_kcompat,pfq_symvers,proc_cpuinfo :: String    
+pfq_omatic_ver,pfq_kcompat,pfq_symvers,proc_cpuinfo :: String
 
 pfq_omatic_ver  = "1.1"
 pfq_kcompat     = "/usr/include/linux/pf_q-kcompat.h"
@@ -37,61 +38,62 @@ pfq_symvers     = "/lib/modules/" ++ uname_r ++ "/kernel/net/pfq/Module.symvers"
 proc_cpuinfo    = "/proc/cpuinfo"
 
 
-uname_r :: String
-uname_r = head . lines $ unsafePerformIO $ readProcess "/bin/uname" ["-r"] "" 
-
-
 main :: IO ()
 main = do
     putStrLn $ "[PFQ] pfq-omatic: v" ++ pfq_omatic_ver
     sanityCheck
-    getRecursiveContents "." ".c" >>= mapM_ tryPatch 
+    getRecursiveContents "." [".c"] >>= mapM_ tryPatch
     copyFile pfq_symvers "Module.symvers"
     let cmd = "make -j" ++ show getNumberOfPhyCores
     putStrLn $ "[PFQ] compiling: " ++ cmd ++ "..."
     _ <- system cmd
     putStrLn "[PFQ] done."
-                                       
+
+
+uname_r :: String
+uname_r = unsafePerformIO $
+    head . lines <$> readProcess "/bin/uname" ["-r"] ""
+
 
 regexFunCall :: String -> Int -> String
-regexFunCall fun n = fun ++ "[[:space:]]*" ++ "\\(" ++ args n  ++ "\\)"
-                        where args 0 = "[[:space:]]*"
-                              args 1 = "[^,]*"
-                              args x = "[^,]*," ++ args (x-1)
+regexFunCall fun n =
+    fun ++ "[[:space:]]*" ++ "\\(" ++ args n  ++ "\\)"
+        where args 0 = "[[:space:]]*"
+              args 1 = "[^,]*"
+              args x = "[^,]*," ++ args (x-1)
 
 
 tryPatch :: FilePath -> IO ()
-tryPatch file = 
+tryPatch file =
     readFile file >>= \c ->
-        when (c =~ (regexFunCall "netif_rx" 1 ++ "|" ++ regexFunCall "netif_receive_skb" 1 ++ "|" ++ regexFunCall "napi_gro_receive" 2)) $ 
-            doesFileExist (file ++ ".orig") >>= \orig -> 
-                if orig 
+        when (c =~ (regexFunCall "netif_rx" 1 ++ "|" ++ regexFunCall "netif_receive_skb" 1 ++ "|" ++ regexFunCall "napi_gro_receive" 2)) $
+            doesFileExist (file ++ ".orig") >>= \orig ->
+                if orig
                 then putStrLn $ "[PFQ] " ++ file ++ " is already patched :)"
                 else makePatch file
-                
-            
+
+
 makePatch :: FilePath -> IO ()
 makePatch file = do
     putStrLn $ "[PFQ] patching " ++ file
     src <- readFile file
     renameFile file $ file ++ ".orig"
-    writeFile file $ "#include " ++ show pfq_kcompat ++ "\n" ++ src 
+    writeFile file $ "#include " ++ show pfq_kcompat ++ "\n" ++ src
 
 
 sanityCheck :: IO ()
 sanityCheck = do
-    doesFileExist pfq_kcompat >>= \kc -> 
+    doesFileExist pfq_kcompat >>= \kc ->
         unless kc $ error "error: could not locate pfq-kcompat header!"
-    doesFileExist pfq_symvers >>= \sv -> 
-        unless sv $ error "error: could not locate pfq Module.symvers!" 
-    doesFileExist "Makefile"  >>= \mf -> 
-        unless mf $ error "error: Makefile not found!" 
+    doesFileExist pfq_symvers >>= \sv ->
+        unless sv $ error "error: could not locate pfq Module.symvers!"
+    doesFileExist "Makefile"  >>= \mf ->
+        unless mf $ error "error: Makefile not found!"
 
 
 type Ext = String
 
-
-getRecursiveContents :: FilePath -> Ext -> IO [FilePath]
+getRecursiveContents :: FilePath -> [Ext] -> IO [FilePath]
 getRecursiveContents topdir ext = do
   names <- getDirectoryContents topdir
   let properNames = filter (`notElem` [".", ".."]) names
@@ -100,13 +102,13 @@ getRecursiveContents topdir ext = do
     isDirectory <- doesDirectoryExist path
     if isDirectory
       then getRecursiveContents path ext
-      else return $ if takeExtensions path == ext 
+      else return $ if takeExtensions path `elem` ext
                     then [path]
                     else []
   return (concat paths)
- 
+
 
 getNumberOfPhyCores :: Int
-getNumberOfPhyCores = unsafePerformIO $ readFile proc_cpuinfo >>= \file ->  
-    return $ length $ filter (isInfixOf "processor") $ lines file 
+getNumberOfPhyCores = unsafePerformIO $
+    length . (filter (isInfixOf "processor")) . lines <$> readFile proc_cpuinfo
 
