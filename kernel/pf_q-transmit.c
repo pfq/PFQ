c0c6652908d083a9cb8a05ed6321686c4c875f39
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-02-13 16:16:13 +0100

    Minor: removed net_warn_ratelimited()

--- kernel/pf_q-transmit.c
+++ kernel/pf_q-transmit.c
@@ -36,9 +36,9 @@
 static inline u16 pfq_dev_cap_txqueue(struct net_device *dev, u16 queue_index)
 {
         if (unlikely(queue_index >= dev->real_num_tx_queues)) {
-                net_warn_ratelimited("%s selects TX queue %d, but real number of TX queues is %d\n",
-                                     dev->name, queue_index,
-                                     dev->real_num_tx_queues);
+                /* net_warn_ratelimited("%s selects TX queue %d, but real number of TX queues is %d\n", */
+                /*                      dev->name, queue_index, */
+                /*                      dev->real_num_tx_queues); */
                 return 0;
         }
         return queue_index;
