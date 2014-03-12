d07db54eed63b169f4fc1bbf0cfeda3808eeb9d9
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-03-12 16:03:30 +0100

    Added a first implementation of utility functions

--- kernel/pf_q-functional.c
+++ kernel/pf_q-functional.c
@@ -21,33 +21,171 @@
  *
  ****************************************************************/
 
+#include <linux/kernel.h>
 #include <linux/printk.h>
+
+#include <asm/uaccess.h>
 
 #include <linux/pf_q.h>
 #include <linux/pf_q-fun.h>
 
 #include <pf_q-functional.h>
+#include <pf_q-factory.h>
 
-struct pfq_exec_prog * pfq_fun_prog_compile(const struct pfq_fun_prog *source)
+
+size_t pfq_full_context_size(const struct pfq_meta_prog *prog)
 {
-        /* TODO */
-        return NULL;
+        size_t size = 0, n = 0;
+
+        for(; n < prog->size; n++)
+        {
+                size += ALIGN(prog->fun[n].context.size, 8);
+        }
+
+        return size;
 }
 
-void pfq_fun_prog_print(const struct pfq_fun_prog *source)
+int pfq_meta_prog_compile(const struct pfq_meta_prog *prog, struct pfq_exec_prog **exec, void **ctx)
+{
+        size_t mem = sizeof(int) + sizeof(pfq_exec_t) * prog->size;
+        size_t cs  = pfq_full_context_size(prog);
+        char * ptr;
+        int n;
+
+        *exec = (struct pfq_exec_prog *)kzalloc(mem, GFP_KERNEL);
+
+        if (!*exec) {
+                printk(KERN_INFO "[PFQ] meta_prog_compile: no memory (%zu bytes)\n", mem);
+                return -ENOMEM;
+        }
+
+        *ctx = NULL;
+        if (cs) {
+                *ctx  = kmalloc(cs, GFP_KERNEL);
+                if (!*ctx) {
+                        printk(KERN_INFO "[PFQ] meta_prog_compile: no memory (%zu bytes)\n", mem);
+                        return -ENOMEM;
+                }
+        }
+
+        ptr = *ctx;
+
+        (*exec)->size = prog->size;
+
+        for(n = 0; n < prog->size; n++)
+        {
+                (*exec)->fun[n].fun_ptr = pfq_get_function(prog->fun[n].name);
+
+                if (prog->fun[n].context.size) {
+                        (*exec)->fun[n].ctx_ptr = ptr;
+                        memcpy(ptr, prog->fun[n].context.addr, prog->fun[n].context.size);
+                        ptr += ALIGN(prog->fun[n].context.size, 8);
+                }
+                else {
+                        (*exec)->fun[n].ctx_ptr = NULL;
+                }
+
+                spin_lock_init(&(*exec)->fun[n].ctx_lock);
+
+                if ((*exec)->fun[n].fun_ptr == NULL) {
+                        pr_devel("[PFQ function error: '%s' unknown function!\n", prog->fun[n].name);
+                        return -EINVAL;
+                }
+        }
+
+        return 0;
+}
+
+
+struct pfq_meta_prog *
+kzalloc_meta_prog(size_t size)
+{
+        size_t mem = sizeof(int) + sizeof(pfq_fun_t) * size;
+        struct pfq_meta_prog *prog = (struct pfq_meta_prog *)kzalloc(mem, GFP_KERNEL);
+
+        if (!prog) {
+                printk(KERN_INFO "[PFQ] kmalloc_meta_prog: no memory (%zu bytes)\n", mem);
+                return NULL;
+        }
+
+        prog->size = size;
+        return prog;
+}
+
+
+void
+kfree_meta_prog(struct pfq_meta_prog *prog)
 {
         int n;
-        for(n = 0; n < source->size; n++)
+        for(n = 0; n < prog->size; n++)
         {
-                pr_devel("    %d: %s (%p,%d)\n", n, source->fun[n].name, source->fun[n].context.addr, source->fun[n].context.size);
+                kfree(prog->fun[n].name);
+                kfree(prog->fun[n].context.addr);
+        }
+        kfree(prog);
+}
+
+
+char *strcat_user(const char __user *str)
+{
+        size_t len = strlen_user(str);
+        char * ret = (char *)kmalloc(len, GFP_KERNEL);
+        if (!ret)
+                return NULL;
+        if (copy_from_user(ret, str, len)) {
+                kfree(ret);
+                return NULL;
+        }
+        return ret;
+}
+
+
+int
+copy_meta_prog_from_user(struct pfq_meta_prog *to, struct pfq_user_meta_prog *from)
+{
+        int n;
+
+        to->size = from->size;
+        for(n = 0; n < to->size; n++)
+        {
+                int csize;
+
+                to->fun[n].name = strcat_user(from->fun[n].name);
+                if (!to->fun[n].name)
+                        return -ENOMEM;
+
+                csize = from->fun[n].context.size;
+                if (csize) {
+                        to->fun[n].context.size = csize;
+                        to->fun[n].context.addr = kmalloc(csize, GFP_KERNEL);
+                        if (copy_from_user(to->fun[n].context.addr, from->fun[n].context.addr, csize))
+                                return -EFAULT;
+                }
+        }
+
+        return 0;
+}
+
+
+void pfq_exec_prog_pr_devel(const struct pfq_exec_prog *prog, const void *ctx)
+{
+        int n;
+        pr_devel("[PFQ] exec program @%p, context@%p:\n", prog, ctx);
+        for(n = 0; n < prog->size; n++)
+        {
+                pr_devel("   %d: f:%p c:%p\n", n, prog->fun[n].fun_ptr, prog->fun[n].ctx_ptr);
         }
 }
 
-void pfq_exec_prog_print(const struct pfq_exec_prog *source)
+
+void pfq_meta_prog_pr_devel(const struct pfq_meta_prog *prog)
 {
         int n;
-        for(n = 0; n < source->size; n++)
+        pr_devel("[PFQ] meta program @%p:\n", prog);
+        for(n = 0; n < prog->size; n++)
         {
-                pr_devel("    %d: f:%p c:%p\n", n, source->step[n].ptr_fun, source->step[n].ptr_ctx);
+                pr_devel("   %d: %s (%p,%d)\n", n, prog->fun[n].name, prog->fun[n].context.addr, prog->fun[n].context.size);
         }
 }
+
+
--- kernel/pf_q-functional.h
+++ kernel/pf_q-functional.h
@@ -30,9 +30,9 @@
 
 typedef struct pfq_exec
 {
-        void *  ptr_fun;
-        void *  ptr_ctx;
-        long    lock_ctx;
+        void *  fun_ptr;
+        void *  ctx_ptr;
+        spinlock_t ctx_lock;
 
 } pfq_exec_t;
 
@@ -40,13 +40,25 @@ typedef struct pfq_exec
 struct pfq_exec_prog
 {
         int size;
-        pfq_exec_t step[];
+        pfq_exec_t fun[];
 };
 
-extern struct pfq_exec_prog * pfq_fun_prog_compile(const struct pfq_fun_prog *source);
 
-extern void   pfq_fun_prog_print(const struct pfq_fun_prog *source);
-extern void   pfq_exec_prog_print(const struct pfq_exec_prog *source);
+static inline size_t
+pfq_meta_prog_memsize(size_t size)
+{
+        return sizeof(int) + sizeof(pfq_fun_t) * size;
+}
 
+
+extern struct pfq_meta_prog * kzalloc_meta_prog(size_t size);
+void  kfree_meta_prog(struct pfq_meta_prog *prog);
+
+extern int copy_meta_prog_from_user(struct pfq_meta_prog *to, struct pfq_user_meta_prog *from);
+
+extern int pfq_meta_prog_compile(const struct pfq_meta_prog *prog, struct pfq_exec_prog **exec, void **ctx);
+
+extern void  pfq_exec_prog_pr_devel(const struct pfq_exec_prog *prog, const void *ctx);
+extern void  pfq_meta_prog_pr_devel(const struct pfq_meta_prog *prog);
 
 #endif /* _PF_Q_FUNCTIONAL_H_ */
