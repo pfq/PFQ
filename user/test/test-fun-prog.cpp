d8309f4b6ccd3b844d27397057c71f1aa67aa5b9
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-03-14 13:29:01 +0100

    test added

---
+++ user/test/test-fun-prog.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <chrono>
+
+#include <pfq.hpp>
+using namespace net;
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 2)
+        throw std::runtime_error(std::string("usage: ").append(argv[0]).append(" dev"));
+
+    pfq q(128);
+
+    q.bind(argv[1], pfq::any_queue);
+
+    auto gid = q.group_id();
+
+    auto prog = reinterpret_cast<pfq_meta_prog *>(malloc(sizeof(int) + sizeof(pfq_fun_t) * 3));
+
+    prog->size = 2;
+
+    prog->fun[0].name = "icmp";
+    prog->fun[0].context.addr = NULL;
+    prog->fun[0].context.size = 0;
+
+    prog->fun[1].name = "steer-ip";
+    prog->fun[1].context.addr = NULL;
+    prog->fun[1].context.size = 0;
+
+    q.set_group_program(gid, prog);
+
+    q.enable();
+
+    for(;;)
+    {
+            auto many = q.read( 1000000 /* timeout: micro */);
+
+            queue::iterator it = many.begin();
+            queue::iterator it_e = many.end();
+
+            for(; it != it_e; ++it)
+            {
+                    while (!it.ready()) {
+                        std::this_thread::yield();
+                    }
+
+                    printf("vlan:%d caplen:%d len:%d ifindex:%d hw_queue:%d -> ",
+                           it->un.vlan.vlan_vid, it->caplen, it->len, it->if_index, it->hw_queue);
+
+                    char *buff = static_cast<char *>(it.data());
+
+                    for(int x=0; x < std::min<int>(it->caplen, 34); x++)
+                    {
+                        printf("%2x ", (unsigned char)buff[x]);
+                    }
+                    printf("\n");
+            }
+    }
+
+    return 0;
+}
+
