5d2bf78b1feeff3885fde343f45e6ba8977b784e
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-03-19 15:18:47 +0100

    In sparse counter, minor changes.

--- kernel/pf_q-sparse-counter.h
+++ kernel/pf_q-sparse-counter.h
@@ -28,13 +28,14 @@
 #include <linux/smp.h>  /* get_cpu */
 #include <asm/local.h>
 
-#define MAX_CPU_CTX     64
+
+#define Q_MAX_CPU     16
 
 
 typedef struct { local_t value; } __attribute__((aligned(64))) counter_t;
 
 
-typedef struct { counter_t ctx[MAX_CPU_CTX]; } sparse_counter_t;
+typedef struct { counter_t ctx[Q_MAX_CPU]; } sparse_counter_t;
 
 
 static inline
@@ -96,7 +97,7 @@ static inline
 void sparse_set(sparse_counter_t *sc, long n)
 {
         unsigned int i, me = get_cpu();
-        for(i = 0; i < MAX_CPU_CTX; i++)
+        for(i = 0; i < Q_MAX_CPU; i++)
                 local_set(&sc->ctx[i].value, i == me ? n : 0);
         put_cpu();
 }
@@ -105,7 +106,7 @@ static inline
 long sparse_read(sparse_counter_t *sc)
 {
         long ret = 0; int i;
-        for(i=0; i < MAX_CPU_CTX; i++)
+        for(i=0; i < Q_MAX_CPU; i++)
                 ret += local_read(&sc->ctx[i].value);
 
         return ret;
