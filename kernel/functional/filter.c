26f847ca9861ad1ec4ee8dfaaf89a58774dd6bf8
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-03-25 15:30:22 +0100

    Simple filters implemented in term of predicates

--- kernel/functional/filter.c
+++ kernel/functional/filter.c
@@ -24,6 +24,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+#include <pf_q-predicate.h>
 
 #include <linux/pf_q-module.h>
 
@@ -38,163 +39,49 @@ id(context_t ctx, struct sk_buff *skb)
 static struct sk_buff *
 filter_ip(context_t ctx, struct sk_buff *skb)
 {
-	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
-	{
-		struct iphdr _iph;
-    		const struct iphdr *ip;
-
-		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
- 		if (ip)
-                {
-                        return cont(skb);
-                }
-        }
-
-	return drop(skb);
+        return is_ip(skb) ? skb : drop(skb);
 }
 
 
 static struct sk_buff *
 filter_ipv6(context_t ctx, struct sk_buff *skb)
 {
-	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IPV6))
-	{
-		struct ipv6hdr _ip6h;
-    		const struct ipv6hdr *ip6;
-
-		ip6 = skb_header_pointer(skb, skb->mac_len, sizeof(_ip6h), &_ip6h);
- 		if (ip6)
-                        return cont(skb);
-	}
-
-        return drop(skb);
+        return is_ipv6(skb) ? skb : drop(skb);
 }
 
 
 static struct sk_buff *
 filter_udp(context_t ctx, struct sk_buff *skb)
 {
-	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
-	{
-		struct iphdr _iph;
-    		const struct iphdr *ip;
-
-		struct udphdr _udp;
-		const struct udphdr *udp;
-
-		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
- 		if (ip == NULL)
- 			return drop(skb);
-
-		if (ip->protocol != IPPROTO_UDP)
-			return drop(skb);
-
-		udp = skb_header_pointer(skb, skb->mac_len + (ip->ihl<<2), sizeof(_udp), &_udp);
-		if (udp)
-		{
-		        return cont(skb);
-                }
-	}
-
-	return drop(skb);
+        return is_udp(skb) ? skb : drop(skb);
 }
 
 
 static struct sk_buff *
 filter_tcp(context_t ctx, struct sk_buff *skb)
 {
-	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
-	{
-		struct iphdr _iph;
-    		const struct iphdr *ip;
-
-		struct tcphdr _tcp;
-		const struct tcphdr *tcp;
-
-		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
- 		if (ip == NULL)
-	                return drop(skb);
-
-		if (ip->protocol != IPPROTO_TCP)
-	                return drop(skb);
-
-		tcp = skb_header_pointer(skb, skb->mac_len + (ip->ihl<<2), sizeof(_tcp), &_tcp);
-		if (tcp)
-                {
-                        return cont(skb);
-                }
-	}
-
-        return drop(skb);
+        return is_tcp(skb) ? skb : drop(skb);
 }
 
 
 static struct sk_buff *
 filter_icmp(context_t ctx, struct sk_buff *skb)
 {
-	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
-	{
-		struct iphdr _iph;
-    		const struct iphdr *ip;
-
-		struct icmphdr _icmp;
-		const struct icmphdr *icmp;
-
-		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
- 		if (ip == NULL)
-	                return drop(skb);
-
-		if (ip->protocol != IPPROTO_ICMP)
-	                return drop(skb);
-
-		icmp = skb_header_pointer(skb, skb->mac_len + (ip->ihl<<2), sizeof(_icmp), &_icmp);
-		if (icmp)
-		{
-                        return cont(skb);
-                }
-	}
-
-        return drop(skb);
+        return is_icmp(skb) ? skb : drop(skb);
 }
 
 
 static struct sk_buff *
 filter_flow(context_t ctx, struct sk_buff *skb)
 {
-	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
-	{
-		struct iphdr _iph;
-    		const struct iphdr *ip;
-
-		struct udphdr _udp;
-		const struct udphdr *udp;
-
-		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
- 		if (ip == NULL)
- 			return drop(skb);
-
-		if (ip->protocol != IPPROTO_UDP &&
-		    ip->protocol != IPPROTO_TCP)
-			return drop(skb);
-
-		udp = skb_header_pointer(skb, skb->mac_len + (ip->ihl<<2), sizeof(_udp), &_udp);
-		if (udp)
-		{
-		        return cont(skb);
-                }
-	}
-
-       	return drop(skb);
+        return has_flow(skb) ? skb : drop(skb);
 }
 
 
 static struct sk_buff *
 filter_vlan(context_t ctx, struct sk_buff *skb)
 {
-        if ((skb->vlan_tci & VLAN_VID_MASK) == 0)
-                return drop(skb);
-        else
-                return cont(skb);
+        return has_vlan(skb) ? skb : drop(skb);
 }
 
 
