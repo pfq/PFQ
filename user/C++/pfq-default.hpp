10c767ae6c1fbdb55a829e3f541d152083907a2d
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-04-19 14:50:53 +0200

    pfq-default and related test added.
    asdfasdfasdf

---
+++ user/C++/pfq-default.hpp
@@ -0,0 +1,151 @@
+/***************************************************************
+ *
+ * (C) 2014 Nicola Bonelli <nicola.bonelli@cnit.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ ****************************************************************/
+
+#pragma once
+
+#include <pfq-lang.hpp>
+
+namespace pfq_lang
+{
+    // default combinators:
+
+    template <typename P1, typename P2>
+    auto inline operator&(P1 const &p1, P2 const &p2)
+    -> decltype(predicate2(combinator(nullptr), p1, p2))
+    {
+        return predicate2(combinator("and"), p1, p2);
+    }
+
+    template <typename P1, typename P2>
+    auto inline operator|(P1 const &p1, P2 const &p2)
+    -> decltype(predicate2(combinator(nullptr), p1, p2))
+    {
+        return predicate2(combinator("or"), p1, p2);
+    }
+
+    template <typename P1, typename P2>
+    auto inline operator^(P1 const &p1, P2 const &p2)
+    -> decltype(predicate2(combinator(nullptr), p1, p2))
+    {
+        return predicate2(combinator("xor"), p1, p2);
+    }
+
+    // polymorphic lambdaassssss.... waiting for C++14! In the meanwhile...
+    //
+
+    struct hpred
+    {
+        hpred(std::string name)
+        : name_(std::move(name))
+        {}
+
+        std::string name_;
+
+        template <typename P>
+        auto operator()(P const &p)
+        -> decltype(hcomputation(name_, p))
+        {
+            return hcomputation(name_, p);
+        }
+    };
+
+    struct hpred1
+    {
+        hpred1(std::string name)
+        : name_(std::move(name))
+        {}
+
+        std::string name_;
+
+        template <typename P, typename C>
+        auto operator()(P const &p, C const &c)
+        -> decltype(hcomputation1(name_, p, c))
+        {
+            return hcomputation1(name_, p, c);
+        }
+    };
+
+    struct hpred2
+    {
+        hpred2(std::string name)
+        : name_(std::move(name))
+        {}
+
+        std::string name_;
+
+        template <typename P, typename C1, typename C2>
+        auto operator()(P const &p, C1 const &c1, C2 const &c2)
+        -> decltype(hcomputation2(name_, p, c1, c2))
+        {
+            return hcomputation2(name_, p, c1, c2);
+        }
+    };
+
+    namespace
+    {
+        // default predicates:
+
+        auto is_ip    = predicate ("is_ip");
+        auto is_ip6   = predicate ("is_ip6");
+        auto is_udp   = predicate ("is_udp");
+        auto is_tcp   = predicate ("is_tcp");
+        auto is_icmp  = predicate ("is_icmp");
+        auto has_mark = [] (unsigned long value) { return predicate1("has_mark", value); };
+
+        // default computations:
+
+        auto steer_mac  = computation("steer_mac");
+        auto steer_vlan = computation("steer_vid");
+        auto steer_ip   = computation("steer_ip");
+        auto steer_ip6  = computation("steer_ip6");
+        auto steer_flow = computation("steer_flow");
+        auto steer_rtp  = computation("steer_rtp");
+
+        // others:
+
+        auto ip   = computation("ip");
+        auto ip6  = computation("ip6");
+        auto udp  = computation("udp");
+        auto tcp  = computation("tcp");
+        auto vlan = computation("vlan");
+        auto icmp = computation("icmp");
+        auto flow = computation("flow");
+        auto rtp  = computation("rtp");
+
+        auto broadcast  = computation("broadcast");
+        auto legacy     = computation("legacy");
+        auto sink       = computation("sink");
+        auto drop       = computation("drop");
+        auto id         = computation("id");
+
+        auto mark       = [] (unsigned long value) { return computation1("counter", value); };
+        auto dummy      = [] (int value) { return computation1("dummy", value); };
+        auto counter    = [] (int value) { return computation1("counter", value); };
+        auto class_     = [] (int value) { return computation1("class", value); };
+
+        auto hdummy      = hpred("hdummy");
+        auto when        = hpred1("when");
+        auto unless      = hpred1("unless");
+        auto conditional = hpred2("conditional");
+    }
+}
---
+++ user/test/test-lang-default.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "pfq-default.hpp"
+
+using namespace pfq_lang;
+
+int
+main()
+{
+    // predicates and combinators:
+
+    std::cout << show (is_tcp) << std::endl;
+    std::cout << show (has_mark(42)) << std::endl;
+    std::cout << show ( is_ip  & is_tcp ) << std::endl;
+    std::cout << show ( is_udp | is_tcp ) << std::endl;
+    std::cout << show ( is_ip6 ^ has_mark(11) ) << std::endl;
+    std::cout << show ( is_ip & ( is_tcp | is_udp) ) << std::endl;
+
+    // computations:
+
+    std::cout << show ( ip >> udp >> steer_rtp >> counter(2) ) << std::endl;
+
+    std::cout << show ( hdummy (is_ip) ) << std::endl;
+    std::cout << show ( hdummy (is_ip) ) << std::endl;
+
+    std::cout << show ( when   (is_ip, ip >> steer_ip) ) << std::endl;
+    std::cout << show ( unless (is_ip, ip >> steer_ip) ) << std::endl;
+    std::cout << show ( conditional (is_ip, steer_ip, drop  ) ) << std::endl;
+
+    return 0;
+}
+
