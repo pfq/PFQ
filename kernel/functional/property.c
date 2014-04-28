9ca87341af302f62f019fc482eb2a6d3987319fd
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-04-28 07:14:50 +0200

    Simple ip_tos, ip_tot_len and ip_id properties added.

--- kernel/functional/property.c
+++ kernel/functional/property.c
@@ -25,18 +25,71 @@
 #include <linux/module.h>
 #include <linux/pf_q-module.h>
 
-#include "predicate.h"
+#include "inline.h"
 
 
 static uint64_t
-ip_tos(arguments_t * a, struct sk_buff const *skb)
+ip_tos(arguments_t *a, struct sk_buff const *skb)
 {
-	return 0;
+	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
+	{
+		struct iphdr _iph;
+    		const struct iphdr *ip;
+
+		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
+ 		if (ip == NULL)
+                        return NOTHING;
+
+		return JUST(ip->tos);
+	}
+
+        return NOTHING;
 }
+
+
+static uint64_t
+ip_tot_len(arguments_t *a, struct sk_buff const *skb)
+{
+	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
+	{
+		struct iphdr _iph;
+    		const struct iphdr *ip;
+
+		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
+ 		if (ip == NULL)
+                        return NOTHING;
+
+		return JUST(ntohs(ip->tot_len));
+	}
+
+        return NOTHING;
+}
+
+
+static uint64_t
+ip_id(arguments_t *a, struct sk_buff const *skb)
+{
+	if (eth_hdr(skb)->h_proto == __constant_htons(ETH_P_IP))
+	{
+		struct iphdr _iph;
+    		const struct iphdr *ip;
+
+		ip = skb_header_pointer(skb, skb->mac_len, sizeof(_iph), &_iph);
+ 		if (ip == NULL)
+                        return NOTHING;
+
+		return JUST(ntohs(ip->id));
+	}
+
+        return NOTHING;
+}
+
 
 struct pfq_property_fun_descr property_functions[] = {
 
-        { "ip_tos", 	 ip_tos 	, FUN_PROPERTY },
+        { "ip_tos", 	ip_tos 	  	, FUN_PROPERTY },
+        { "ip_tot_len", ip_tot_len	, FUN_PROPERTY },
+        { "ip_id",  	ip_id 	  	, FUN_PROPERTY },
 
         { NULL, NULL}};
 
