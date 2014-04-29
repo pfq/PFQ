5dc6725358b9f768352e30c1384adb71eca430f0
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-04-29 16:31:05 +0200

    Tests updated.

--- user/test/test-fun-lang.cpp
+++ user/test/test-fun-lang.cpp
@@ -28,7 +28,7 @@ main(int argc, char *argv[])
     //     drop'
 
     auto comp = ip >> addr("192.168.0.0", 16) >> counter (0) >>
-                    conditional (is_icmp & has_addr("192.168.0.0", 16),
+                    conditional (is_icmp & has_addr("192.168.0.0", 16) & any_bit(ip_id, 0xffffff),
                                  (counter (1) >> mark (1) >> steer_ip >> when (has_mark (1), counter (2))),
                                   drop);
 
