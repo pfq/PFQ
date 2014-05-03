9d15eb7b8db4a58dcc730bc888e489d5abc59fcf
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-05-03 14:14:27 +0200

    Added functional/headers.h for inline functions

---
+++ kernel/functional/headers.h
@@ -0,0 +1,34 @@
+/***************************************************************
+ *
+ * (C) 2011-14 Nicola Bonelli <nicola.bonelli@cnit.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ ****************************************************************/
+
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
+#include <functional/combinator.h>
+#include <functional/conditional.h>
+#include <functional/predicate.h>
+#include <functional/forward.h>
+#include <functional/filter.h>
+#include <functional/misc.h>
+
+#endif /* _HEADERS_H_ */
