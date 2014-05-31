46d86c1a414102bab2340bb00e91ba62244892e5
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-05-31 12:32:59 +0200

    Test updated.

--- user/Haskell/test-newlang.hs
+++ user/Haskell/test-newlang.hs
@@ -1,7 +1,12 @@
 import Network.PFq.Lang
 import Network.PFq.Default
 
+import Data.Typeable
+
 main = do
         let p = ip >-> tcp >-> (conditional is_ip drop' steer_ip ) >-> class' 10
 
+        putStrLn $ pretty p
         putStrLn $ show p
+        putStrLn $ show $ typeOf(port)
+        putStrLn $ typeOf'(port)
