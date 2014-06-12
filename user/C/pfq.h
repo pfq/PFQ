9977fbbb561dafb1ba5c20ebe6daaff8ac905514
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-06-12 17:54:52 +0200

    Few comments fixed.

--- user/C/pfq.h
+++ user/C/pfq.h
@@ -143,7 +143,7 @@ typedef void (*pfq_handler_t)(char *user, const struct pfq_pkt_hdr *h, const cha
  * ************************************** */
 
 /*!
- * group_policy: Q_GROUP_PRIVATE, Q_GROUP_RESTRICTED, Q_GROUP_SHARED, Q_GROUP_UNDEFINED...
+ * group_policy: Q_POLICY_GROUP_PRIVATE, Q_POLICY_GROUP_RESTRICTED, Q_POLICY_GROUP_SHARED, Q_POLICY_GROUP_UNDEFINED...
  *
  * class_mask  : Q_CLASS_DEFAULT| .... = Q_CLASS_ANY
  */
@@ -160,7 +160,7 @@ extern pfq_t* pfq_open_default();
 /*! Open the socket and create a new private group. */
 /*!
  * The default values for class mask and group policy are Q_CLASS_DEFAULT and
- * Q_GROUP_PRIVATE, respectively.
+ * Q_POLICY_GROUP_PRIVATE, respectively.
  */
 
 extern pfq_t* pfq_open(size_t calpen, size_t slots);
@@ -169,7 +169,7 @@ extern pfq_t* pfq_open(size_t calpen, size_t slots);
 /*! Open the socket and create a new private group. */
 /*!
  * The default values for class mask and group policy are Q_CLASS_DEFAULT and
- * Q_GROUP_PRIVATE, respectively.
+ * Q_POLICY_GROUP_PRIVATE, respectively.
  */
 
 extern pfq_t *pfq_open_(size_t caplen, size_t rx_slots, size_t maxlen, size_t tx_slots);
