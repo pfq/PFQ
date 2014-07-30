4382d8d22ec7dda15c5561c81d5d6368cd1b6362
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-07-30 16:27:16 +0200

    pfq-lang simplified implementation: single thread.

--- user/tool/pfq-lang.cpp
+++ user/tool/pfq-lang.cpp
@@ -39,120 +39,11 @@ using namespace pfq::lang;
 
 namespace opt
 {
-    int sleep_microseconds;
     std::string function;
 
     size_t caplen = 64;
     size_t slots  = 131072;
-    bool flow     = false;
-}
-
-
-namespace thread
-{
-    struct context
-    {
-        context(int id, const binding &b)
-        : m_id(id)
-        , m_bind(b)
-        , m_stop(std::unique_ptr<std::atomic_bool>(new std::atomic_bool(false)))
-        , m_pfq(group_policy::undefined, opt::caplen, opt::slots)
-        , m_read()
-        , m_batch()
-        {
-            if (m_bind.gid == -1)
-                m_bind.gid = id;
-
-            m_pfq.join_group(m_bind.gid, group_policy::shared);
-
-            for(auto &d : m_bind.dev)
-            {
-                if (m_bind.queue.empty())
-                {
-                    m_pfq.bind_group(m_bind.gid, d.c_str(), -1);
-                    std::cout << "+ bind to " << d << "@" << -1 << std::endl;
-                }
-                else
-                    for(auto q : m_bind.queue)
-                    {
-                        std::cout << "+ bind to " << d << "@" << q << std::endl;
-                        m_pfq.bind_group(m_bind.gid, d.c_str(), q);
-                    }
-            }
-
-            if (!opt::function.empty() && (m_id == 0))
-            {
-                std::cout << "fun: " << opt::function << std::endl;
-                m_pfq.set_group_computation(m_bind.gid, opt::function);
-            }
-            else
-            {
-                std::cout << "fun: " << pretty(computation) << std::endl;
-                m_pfq.set_group_computation(m_bind.gid, computation);
-            }
-
-            m_pfq.timestamp_enable(false);
-
-            m_pfq.enable();
-        }
-
-        context(const context &) = delete;
-        context& operator=(const context &) = delete;
-
-        context(context &&) = default;
-        context& operator=(context &&) = default;
-
-        void operator()()
-        {
-            for(;;)
-            {
-                auto many = m_pfq.read(opt::sleep_microseconds);
-
-                m_read += many.size();
-
-                m_batch = std::max(m_batch, many.size());
-
-                if (m_stop->load(std::memory_order_relaxed))
-                    return;
-            }
-        }
-
-        void stop()
-        {
-            m_stop->store(true, std::memory_order_release);
-        }
-
-        pfq_stats
-        stats() const
-        {
-            return m_pfq.stats();
-        }
-
-        unsigned long long
-        read() const
-        {
-            return m_read;
-        }
-
-        size_t
-        batch() const
-        {
-            return m_batch;
-        }
-
-    private:
-        int m_id;
-        binding m_bind;
-
-        std::unique_ptr<std::atomic_bool> m_stop;
-
-        pfq::socket m_pfq;
-
-        unsigned long long m_read;
-        size_t m_batch;
-
-    };
-
+    bool   flow   = false;
 }
 
 
@@ -165,7 +56,7 @@ void usage(std::string name)
         " -c --caplen=INT               Set caplen\n"
         " -s --slot=INT                 Set slots\n"
         " -f --function=FUNCTION\n"
-        " -t --thread=BINDING\n\n"
+        " -b --binding=BINDING\n\n"
         "      BINDING = eth0:...:ethx[.core[.gid[.queue.queue...]]]\n"
         "      FUNCTION = fun[ >-> fun >-> fun]"
     );
@@ -179,10 +70,7 @@ try
     if (argc < 2)
         usage(argv[0]);
 
-    std::vector<std::thread> vt;
-    std::vector<thread::context> ctx;
-
-    std::vector<binding> thread_binding;
+    pfq::binding bind;
 
     for(int i = 1; i < argc; ++i)
     {
@@ -228,15 +116,15 @@ try
             continue;
         }
 
-        if ( strcmp(argv[i], "-t") == 0 ||
-             strcmp(argv[i], "--thread") == 0) {
+        if ( strcmp(argv[i], "-b") == 0 ||
+             strcmp(argv[i], "--binding") == 0) {
             i++;
             if (i == argc)
             {
                 throw std::runtime_error("descriptor missing");
             }
 
-            thread_binding.push_back(make_binding(argv[i]));
+            bind = make_binding(argv[i]);
             continue;
         }
 
@@ -254,94 +142,97 @@ try
 
     if (opt::slots < 1024)
     {
-        std::cout << "too few slots may affect the performance!" << std::endl;
+        std::cout << "too few slots may reduce performance!" << std::endl;
         _Exit(0);
     }
 
-    // create thread context:
-    //
-    for(unsigned int i = 0; i < thread_binding.size(); ++i)
+    //////////////////////////////////////////////////////////////////////
+
+    pfq::socket q(group_policy::undefined, opt::caplen, opt::slots);
+
+    if (bind.gid == -1)
+        bind.gid = 1;
+
+    q.join_group(bind.gid, group_policy::shared);
+
+    for(auto &d : bind.dev)
     {
-        ctx.push_back(thread::context(i, thread_binding[i]));
+        if (bind.queue.empty())
+        {
+            q.bind_group(bind.gid, d.c_str(), -1);
+            std::cout << "+ bind to " << d << "@" << -1 << std::endl;
+        }
+        else
+            for(auto bq : bind.queue)
+            {
+                std::cout << "+ bind to " << d << "@" << bq << std::endl;
+                q.bind_group(bind.gid, d.c_str(), bq);
+            }
     }
 
-    opt::sleep_microseconds = 50000 * ctx.size();
-    std::cout << "poll timeout " << opt::sleep_microseconds << " usec" << std::endl;
-
-    // create threads:
-    //
-
-    int i = 0;
-    std::for_each(thread_binding.begin(), thread_binding.end(), [&](binding &b) {
-
-                  std::thread t(std::ref(ctx[i++]));
-
-                  std::cout << "thread: " << show_binding(b) << std::endl;
-
-                  if (b.core != -1)
-                  extra::set_affinity(t, b.core);
-
-                  vt.push_back(std::move(t));
-                  });
-
-    unsigned long long sum, old = 0;
-    pfq_stats sum_stats, old_stats = {0,0,0,0,0};
-
-    std::cout << "----------- capture started ------------\n";
-
-    auto begin = std::chrono::system_clock::now();
-
-    for(int y=0;; y++)
+    if (!opt::function.empty())
     {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        sum = 0;
-        sum_stats = {0,0,0,0,0};
-
-        std::for_each(ctx.begin(), ctx.end(), [&](const thread::context &c) {
-                      sum += c.read();
-                      sum_stats += c.stats();
-                      });
-
-        std::cout << "recv: ";
-        std::for_each(ctx.begin(), ctx.end(), [&](const thread::context &c) {
-                      std::cout << c.stats().recv << ' ';
-                      });
-        std::cout << " -> " << sum_stats.recv << std::endl;
-
-        std::cout << "lost: ";
-        std::for_each(ctx.begin(), ctx.end(), [&](const thread::context &c) {
-                      std::cout << c.stats().lost << ' ';
-                      });
-        std::cout << " -> " << sum_stats.lost << std::endl;
-
-        std::cout << "drop: ";
-        std::for_each(ctx.begin(), ctx.end(), [&](const thread::context &c) {
-                      std::cout << c.stats().drop << ' ';
-                      });
-        std::cout << " -> " << sum_stats.drop << std::endl;
-
-        std::cout << "max_batch: ";
-        std::for_each(ctx.begin(), ctx.end(), [&](const thread::context &c) {
-                      std::cout << c.batch() << ' ';
-                      });
-        std::cout << std::endl;
-
-        auto end = std::chrono::system_clock::now();
-
-        std::cout << "capture: " << vt100::BOLD <<
-        (static_cast<uint64_t>(sum-old)*1000000)/std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count()
-        << vt100::RESET << " pkt/sec";
-
-
-        std::cout << std::endl;
-
-        old = sum, begin = end;
-        old_stats = sum_stats;
+        std::cout << "fun: " << opt::function << std::endl;
+        q.set_group_computation(bind.gid, opt::function);
+    }
+    else
+    {
+        std::cout << "fun: " << pretty(computation) << std::endl;
+        q.set_group_computation(bind.gid, computation);
     }
 
-    std::for_each(ctx.begin(), ctx.end(), std::mem_fn(&thread::context::stop));
-    std::for_each(vt.begin(), vt.end(), std::mem_fn(&std::thread::join));
+    q.timestamp_enable(false);
+
+    q.enable();
+
+    //////////////////////////////////////////////////////////////////////
+
+    std::atomic<unsigned long> read { 0 };
+
+    std::thread stat([&]() {
+
+        std::cout << "----------- capture started ------------\n";
+
+        unsigned long long sum, old = 0;
+        pfq_stats sum_stats, old_stats = {0,0,0,0,0};
+
+        auto begin = std::chrono::system_clock::now();
+
+        for(int y=0;; y++)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+            sum = 0;
+            sum_stats = {0,0,0,0,0};
+
+            sum       += read;
+            sum_stats += q.stats();
+
+            std::cout << "recv: " << sum_stats.recv << std::endl;
+            std::cout << "lost: " << sum_stats.lost << std::endl;
+            std::cout << "drop: " << sum_stats.drop << std::endl;
+
+            auto end = std::chrono::system_clock::now();
+
+            std::cout << "capture: " << vt100::BOLD <<
+                (static_cast<uint64_t>(sum-old)*1000000)/std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count()
+            << vt100::RESET << " pkt/sec";
+
+
+            std::cout << std::endl;
+
+            old = sum, begin = end;
+            old_stats = sum_stats;
+        }
+
+    });
+
+    for(;;)
+    {
+        auto many = q.read(50000);
+
+        read += many.size();
+    }
 
     return 0;
 }
