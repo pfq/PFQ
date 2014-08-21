fb7933f4d72d7281cf9875f63a02ad4e2487de21
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-08-21 11:09:16 +0200

    bloom_src, bloom_dst, bloom_src_filter and bloom_dst_filter added.

--- kernel/functional/bloom.c
+++ kernel/functional/bloom.c
@@ -30,6 +30,73 @@
 
 
 static bool
+bloom_src(arguments_t args, SkBuff b)
+{
+	if (eth_hdr(b.skb)->h_proto == __constant_htons(ETH_P_IP))
+	{
+		struct iphdr _iph;
+    		const struct iphdr *ip;
+		unsigned int mask;
+		char *mem;
+
+		ip = skb_header_pointer(b.skb, b.skb->mac_len, sizeof(_iph), &_iph);
+ 		if (ip == NULL)
+                        return false;
+
+		mem = get_arg_1(char *, args);
+        	mask = get_arg_0(unsigned int, args);
+
+		pr_devel("IP_src = %x\n", ip->saddr);
+		pr_devel("HASH_1 = %x\n", hfun1(ip->saddr));
+		pr_devel("HASH_2 = %x\n", hfun2(ip->saddr));
+		pr_devel("HASH_3 = %x\n", hfun3(ip->saddr));
+		pr_devel("HASH_4 = %x\n", hfun4(ip->saddr));
+
+		if ( BF_TEST(mem, hfun1(ip->saddr) & mask ) &&
+		     BF_TEST(mem, hfun2(ip->saddr) & mask ) &&
+		     BF_TEST(mem, hfun3(ip->saddr) & mask ) &&
+		     BF_TEST(mem, hfun4(ip->saddr) & mask ) )
+		     	return true;
+	}
+
+        return false;
+}
+
+
+static bool
+bloom_dst(arguments_t args, SkBuff b)
+{
+	if (eth_hdr(b.skb)->h_proto == __constant_htons(ETH_P_IP))
+	{
+		struct iphdr _iph;
+    		const struct iphdr *ip;
+		unsigned int mask;
+		char *mem;
+
+		ip = skb_header_pointer(b.skb, b.skb->mac_len, sizeof(_iph), &_iph);
+ 		if (ip == NULL)
+                        return false;
+
+		mem = get_arg_1(char *, args);
+        	mask = get_arg_0(unsigned int, args);
+
+		pr_devel("IP_dst = %x\n", ip->daddr);
+		pr_devel("HASH_1 = %x\n", hfun1(ip->daddr));
+		pr_devel("HASH_2 = %x\n", hfun2(ip->daddr));
+		pr_devel("HASH_3 = %x\n", hfun3(ip->daddr));
+		pr_devel("HASH_4 = %x\n", hfun4(ip->daddr));
+
+		if ( BF_TEST(mem, hfun1(ip->daddr) & mask ) &&
+		     BF_TEST(mem, hfun2(ip->daddr) & mask ) &&
+		     BF_TEST(mem, hfun3(ip->daddr) & mask ) &&
+		     BF_TEST(mem, hfun4(ip->daddr) & mask ) )
+		     	return true;
+	}
+
+        return false;
+}
+
+static bool
 bloom(arguments_t args, SkBuff b)
 {
 	if (eth_hdr(b.skb)->h_proto == __constant_htons(ETH_P_IP))
@@ -67,6 +134,23 @@ static Action_SkBuff
 bloom_filter(arguments_t args, SkBuff b)
 {
 	if (bloom(args, b))
+		return Pass(b);
+	return Drop(b);
+}
+
+
+static Action_SkBuff
+bloom_src_filter(arguments_t args, SkBuff b)
+{
+	if (bloom_src(args, b))
+		return Pass(b);
+	return Drop(b);
+}
+
+static Action_SkBuff
+bloom_dst_filter(arguments_t args, SkBuff b)
+{
+	if (bloom_dst(args, b))
 		return Pass(b);
 	return Drop(b);
 }
@@ -136,6 +220,10 @@ static int bloom_fini(arguments_t args)
 struct pfq_function_descr bloom_functions[] = {
 
         { "bloom",	  	"CInt -> [Word32] -> SkBuff -> Bool", 		bloom, 			bloom_init, 	bloom_fini },
+        { "bloom_src",	  	"CInt -> [Word32] -> SkBuff -> Bool", 		bloom_src, 		bloom_init, 	bloom_fini },
+        { "bloom_dst",	  	"CInt -> [Word32] -> SkBuff -> Bool", 		bloom_dst, 		bloom_init, 	bloom_fini },
         { "bloom_filter", 	"CInt -> [Word32] -> SkBuff -> Action SkBuff", 	bloom_filter, 		bloom_init, 	bloom_fini },
+        { "bloom_src_filter", 	"CInt -> [Word32] -> SkBuff -> Action SkBuff", 	bloom_src_filter, 	bloom_init, 	bloom_fini },
+        { "bloom_dst_filter", 	"CInt -> [Word32] -> SkBuff -> Action SkBuff", 	bloom_dst_filter, 	bloom_init, 	bloom_fini },
         { NULL }};
 
