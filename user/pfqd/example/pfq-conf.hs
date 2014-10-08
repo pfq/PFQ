ad6f97cb6e8256cebfdfda2558a84df695e3baa3
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-10-08 14:37:06 +0200

    In pfqd, pfq-conf.hs example added.

---
+++ user/pfqd/example/pfq-conf.hs
@@ -0,0 +1,17 @@
+module PFQconf where
+
+import Network.PFq.Lang
+import Network.PFq.Default
+import Network.PFq.Experimental
+
+pfq_config =
+    [
+        (1, [Dev "eth0"],
+            ip >-> steer_flow
+        )
+
+        (2, [Dev "eth0", DevQueue "eth1" 1],
+            icmp
+        )
+
+    ] :: [(Integer, [NetDevice], Function (SkBuff -> Action SkBuff))]
