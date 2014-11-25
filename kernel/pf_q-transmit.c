81ff01991ae67ee28adafd1d7387847e7b7c038c
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-11-25 17:36:36 +0100

    PFQ_TX_PROFILE code improved.

--- kernel/pf_q-transmit.c
+++ kernel/pf_q-transmit.c
@@ -93,37 +93,44 @@ int pfq_tx_queue_flush(struct pfq_tx_opt *to, struct net_device *dev, int cpu, i
 
 	struct pfq_tx_queue_hdr *txq = pfq_get_tx_queue_hdr(to);
 
-#ifdef PFQ_TX_PROFILE
-	static int pkt_counter;
-#endif
-
 	/* transmit the batch queue... */
 
 	void pfq_tx_batch_queue(void)
 	{
 		struct sk_buff *skb;
-        	int sent, i;
+        	int len, sent, i;
+
+#ifdef PFQ_TX_PROFILE
+		cycles_t start = get_cycles();
+#endif
+		len = pfq_bounded_queue_len(&skbs);
 
 		sent = pfq_queue_xmit(&skbs, dev, to->hw_queue);
 
                 /* update stats */
 
                 __sparse_add(&to->stats.sent, sent, cpu);
-                __sparse_add(&to->stats.disc, pfq_bounded_queue_len(&skbs) - sent, cpu);
+                __sparse_add(&to->stats.disc, len - sent, cpu);
 
   		__sparse_add(&global_stats.sent, sent, cpu);
-  		__sparse_add(&global_stats.disc, pfq_bounded_queue_len(&skbs) - sent, cpu);
+  		__sparse_add(&global_stats.disc, len - sent, cpu);
 
 		/* free/recycle the packets now... */
 
 		pfq_bounded_queue_for_each(skb, i, &skbs)
-		{
 			pfq_kfree_skb_recycle(skb, &local->tx_recycle_list);
-		}
 
-		pfq_spsc_read_commit_n(txq, pfq_bounded_queue_len(&skbs));
+		pfq_spsc_read_commit_n(txq, len);
 
 		pfq_bounded_queue_flush(&skbs);
+
+#ifdef PFQ_TX_PROFILE
+		if (printk_ratelimit())
+		{
+			cycles_t stop = get_cycles();
+			printk(KERN_INFO "[PFQ] TX avg cpu-cycle: %llu_tsc (batch len = %d).\n", (stop - start)/len, len);
+		}
+#endif
 	}
 
 	pfq_bounded_queue_init(&skbs);
@@ -136,9 +143,6 @@ int pfq_tx_queue_flush(struct pfq_tx_opt *to, struct net_device *dev, int cpu, i
         for(n = 0; n < avail; ++n)
         {
 
-#ifdef PFQ_TX_PROFILE
-		cycles_t start = get_cycles();
-#endif
 		if (unlikely(index >= to->size))
                 {
                         if(printk_ratelimit())
@@ -168,15 +172,7 @@ int pfq_tx_queue_flush(struct pfq_tx_opt *to, struct net_device *dev, int cpu, i
 
                 /* copy bytes in the socket buffer */
 
-#if 1
-                skb_copy_to_linear_data_offset(skb, 0, h+1, len);
-#else
-                if (skb_store_bits(skb, 0, h+1, len) < 0)
-                {
-                        pfq_kfree_skb_recycle(skb, &local->tx_recycle_list);
-                        break;
-                }
-#endif
+                skb_copy_to_linear_data(skb, h+1, len);
 
                 /* take this skb: skb_get */
 
@@ -187,26 +183,16 @@ int pfq_tx_queue_flush(struct pfq_tx_opt *to, struct net_device *dev, int cpu, i
 		pfq_bounded_queue_push(&skbs, skb);
 
 		if (pfq_bounded_queue_len(&skbs) == batch_len)
-		{
 			pfq_tx_batch_queue();
-		}
 
 		/* get the next index... */
 
                 index = pfq_spsc_next_index(txq, index);
 
-#ifdef PFQ_TX_PROFILE
-		if ((pkt_counter++ % 1048576) == 0)
-		{
-			cycles_t stop = get_cycles();
-			printk(KERN_INFO "[PFQ] TX cpu-cycle: %llu_tsc.\n", (stop - start));
-		}
-#endif
         }
 
-	if (pfq_bounded_queue_len(&skbs)) {
+	if (pfq_bounded_queue_len(&skbs))
 		pfq_tx_batch_queue();
-	}
 
         return n;
 }
