e298614f53c3fb5c05550ec1f81e29cf3419f78d
Nicola Bonelli <nicola.bonelli@gmail.com>
2015-01-09 12:44:10 +0100

    test-lang-functional updated.

--- user/test/test-lang-functional.cpp
+++ user/test/test-lang-functional.cpp
@@ -33,47 +33,15 @@ main(int argc, char *argv[])
     //                              (inc (1) >> mark (1) >> steer_ip >> when (has_mark (1), inc (2))),
     //                               drop);
 
-    auto comp = ip >> tee_ ("lo", is_icmp) >> dummy_vector({1,2,3}) >> par(icmp, icmp) >> addr("192.168.0.0", 16)  >> unit >> inc (0) >> log_msg ("This is a test message!") >>
+    auto comp = ip >> tee_ ("lo", is_icmp) >> dummy_vector({1,2,3}) >> par(icmp, icmp) >> addr("192.168.0.1", 16)  >> unit >> inc (0) >> log_msg ("This is a test message!") >>
                     conditional (is_icmp, (inc (1) >> mark (1) >> steer_ip >> when (has_mark (1), inc (2))), drop);
+
 
     std::cout << pretty (comp) << std::endl;
 
     q.set_group_computation(gid, comp);
 
-    q.enable();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    for(;;)
-    {
-            auto many = q.read( 1000000 /* timeout: micro */);
-
-            pfq::queue::iterator it = many.begin();
-            pfq::queue::iterator it_e = many.end();
-
-            for(; it != it_e; ++it)
-            {
-                    while (!it.ready()) {
-                        std::this_thread::yield();
-                    }
-
-                    printf("vlan:%d caplen:%d len:%d ifindex:%d hw_queue:%d -> ",
-                           it->un.vlan.vlan_vid, it->caplen, it->len, it->if_index, it->hw_queue);
-
-                    char *buff = static_cast<char *>(it.data());
-
-                    for(int x=0; x < std::min<int>(it->caplen, 34); x++)
-                    {
-                        printf("%2x ", (unsigned char)buff[x]);
-                    }
-                    printf("\n");
-
-                    auto cs = q.group_counters(gid);
-
-                    printf("counters: { ");
-                    for(auto c : cs)
-                        printf("%lu ", c);
-
-                    printf("}\n");
-            }
-    }
 }
 
