cb631621cb99d626d4281cde7e443cff6543146c
Nicola Bonelli <nicola.bonelli@gmail.com>
2015-02-01 13:34:21 +0100

    Check whether at least a kernel thread is bound before calling tx_async (now required)

--- user/tool/pfq-gen.cpp
+++ user/tool/pfq-gen.cpp
@@ -126,7 +126,10 @@ namespace thread
 
             q.enable();
 
-            q.tx_wakeup();
+            if (std::any_of(std::begin(kcpu), std::end(kcpu), [](int cpu) { return cpu != -1; }))
+            {
+                    q.tx_async(true);
+            }
 
             m_pfq = std::move(q);
         }
