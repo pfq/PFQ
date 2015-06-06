c3aaa4a75bee0ada8e4203548f31ef5e46849c2f
Nicola Bonelli <nicola.bonelli@gmail.com>
2015-06-06 18:10:03 +0200

    Adding restricted user-defined type module.

---
+++ kernel/pf_q-types.h
@@ -0,0 +1,32 @@
+/***************************************************************
+ *
+ * (C) 2011-14 Nicola Bonelli <nicola@pfq.io>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "COPYING".
+ *
+ ****************************************************************/
+
+#ifndef PF_Q_TYPES_H
+#define PF_Q_TYPES_H
+
+typedef int __bitwise pfq_id_t;
+typedef int __bitwise pfq_gid_t;
+
+#define Q_INVALID_ID	(__force pfq_id_t)-1
+
+#endif  /* PF_Q_TYPES_H */
