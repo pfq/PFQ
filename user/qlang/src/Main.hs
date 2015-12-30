df5fff6d52f3b21232dc04643458774a3de71248
Nicola Bonelli <nicola.bonelli@gmail.com>
2015-12-30 12:31:02 +0100

    Exception handling improved.

--- user/qlang/src/Main.hs
+++ user/qlang/src/Main.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Language.Haskell.Interpreter
 import Control.Monad.Trans.Reader
 import System.Console.CmdArgs
 import CmdLine
@@ -7,11 +10,13 @@ import Options
 import System.IO
 
 import Data.Maybe
+import Control.Exception
 
 import qualified QLang.JSON
 import qualified QLang.FDescr
 
 notNull = not . null
+
 
 main = do
   opts <- cmdArgsRun options
@@ -26,12 +31,17 @@ main = do
 
   code <- hGetContents inHandle
 
-  output <- case () of
-            _ | json opts -> runReaderT (QLang.JSON.compile code) opts
-            _ | fdescr opts -> runReaderT (QLang.FDescr.compile code) opts
-              | otherwise -> error "qlang: operation not supported"
+  result <- try $ case () of
+          _ | json opts -> runReaderT (QLang.JSON.compile code) opts
+            | fdescr opts -> runReaderT (QLang.FDescr.compile code) opts
+            | otherwise -> error "qlang: operation not supported"
 
-  hPutStr outHandle output
+  case result of
+    Left (WontCompile es)  ->  error $ unlines (map errMsg es)
+    Left (NotAllowed xs)   ->  error xs
+    Left (GhcException xs) ->  error xs
+    Left (UnknownError xs) ->  error xs
+    Right out -> hPutStr outHandle out
 
   hClose outHandle
   hClose inHandle
