00c5eae205cb5e112ee6f0cba928c48035ae28ac
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-01-02 11:33:42 +0100

    test-lang updated.

--- user/test/test-lang.c
+++ user/test/test-lang.c
@@ -21,7 +21,7 @@ main(int argc, char *argv[])
 
 	printf("testing computation...\n");
 	if (pfq_set_group_computation_from_string(q, pfq_group_id(q),
-			"main = when' is_ip drop' >-> dummy 42 >-> dummy_ip \"192.168.0.1\" >-> dummy_string \"hello world!\"") < 0) {
+			"main = dummy 42 >-> dummy_ip \"192.168.0.1\" >-> dummy_string \"hello world!\" >-> dummy_vector [1,2,3] >-> bloom_filter 1024 [\"192.168.0.1\", \"10.10.10.10\"] 24 ") < 0) {
                 printf("error: %s\n", pfq_error(q));
                 return -1;
 	}
