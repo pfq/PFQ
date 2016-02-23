1122c77d14314c1a762769b4eb9cf6924fb65a8e
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-02-23 09:31:19 +0100

    Test updated.

--- user/test/test-lang-functional.cpp
+++ user/test/test-lang-functional.cpp
@@ -28,13 +28,10 @@ main(int argc, char *argv[])
     //     (inc 1 >-> mark 1 >-> steer_ip >-> when' (has_mark 1) (inc 2))
     //     drop'
 
-    // auto comp = ip >> icmp >> forward ("lo") >> addr("192.168.0.0", 16) >> unit >> inc (0) >>
-    //                 conditional (is_icmp & has_addr("192.168.0.0", 16),
+    // auto comp = ip >> icmp >> forward ("lo") >> addr("192.168.0.0/16") >> unit >> inc (0) >>
+    //                 conditional (is_icmp & has_addr("192.168.0.0/16"),
     //                              (inc (1) >> mark (1) >> steer_ip >> when (has_mark (1), inc (2))),
     //                               drop);
-
-    auto comp = ip >> tee_ ("lo", is_icmp) >> dummy_ip("192.168.1.1") >> dummy_vector({1,2,3}) >> par(icmp, icmp) >> addr("192.168.0.1", 16)  >> unit >> inc (0) >> log_msg ("This is a test message!") >>
-                    conditional (is_icmp, (inc (1) >> mark (1) >> steer_ip >> when (has_mark (1), inc (2))), drop);
 
 
     std::cout << pretty (comp) << std::endl;
