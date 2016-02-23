f7bb41600419058185b1f224a40d4fffd124ef99
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-02-23 10:15:30 +0100

    Minor changes.

--- user/test/test-lang-functional.cpp
+++ user/test/test-lang-functional.cpp
@@ -28,10 +28,10 @@ main(int argc, char *argv[])
     //     (inc 1 >-> mark 1 >-> steer_ip >-> when' (has_mark 1) (inc 2))
     //     drop'
 
-    // auto comp = ip >> icmp >> forward ("lo") >> addr("192.168.0.0/16") >> unit >> inc (0) >>
-    //                 conditional (is_icmp & has_addr("192.168.0.0/16"),
-    //                              (inc (1) >> mark (1) >> steer_ip >> when (has_mark (1), inc (2))),
-    //                               drop);
+    auto comp = ip >> icmp >> forward ("lo") >> addr("192.168.0.0/16") >> unit >> inc (0) >>
+                    conditional (is_icmp & has_addr("192.168.0.0/16"),
+                                 (inc (1) >> mark (1) >> steer_ip >> when (has_mark (1), inc (2))),
+                                  drop);
 
 
     std::cout << pretty (comp) << std::endl;
