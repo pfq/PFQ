1a3bc30e83c7855cdba807ceb783fcb825e39e1f
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-12-05 16:40:34 +0100

    Fix: for kernel 4.6.2

--- kernel/pfq/bpf.c
+++ kernel/pfq/bpf.c
@@ -48,7 +48,7 @@ pfq_alloc_sk_filter(struct sock_fprog *fprog)
 #endif
         pr_devel("[PFQ] BPF: new fprog (len %d)\n", fprog->len);
 
-#if (LINUX_VERSION_CODE == KERNEL_VERSION(4,6,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) && LINUX_VERSION_CODE < KERNEL_VERSION(4,7,0))
 	if ((rv = __sk_attach_filter(fprog, &sk, sock_owned_by_user(&sk))))
 #else
 	if ((rv = sk_attach_filter(fprog, &sk)))
@@ -75,7 +75,7 @@ pfq_free_sk_filter(struct sk_filter *filter)
 	sock_reset_flag(&sk, SOCK_FILTER_LOCKED);
 #endif
 	sk.sk_filter = filter;
-#if (LINUX_VERSION_CODE == KERNEL_VERSION(4,5,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) && LINUX_VERSION_CODE < KERNEL_VERSION(4,7,0))
 	if ((rv = __sk_detach_filter(&sk, sock_owned_by_user(&sk))))
 #else
 	if ((rv = sk_detach_filter(&sk)))
